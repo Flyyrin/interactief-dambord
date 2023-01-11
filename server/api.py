c4729ef6263248cdb326d08543c4bba2ceee4c65
Rafael Lemmen <BouncePlayR@gmail.com>
2023-01-11 10:50:46 +0100

    save

--- server/api.py
+++ server/api.py
@@ -1,9 +1,12 @@
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
 import json
+import os
 
+BASE = os.getcwd()
 GAMES_FILE_PATH = "games.json"
 BLOGS_FILE_PATH = "blogs.json"
+UPLOAD_FOLDER_PATH = os.path.join(BASE, "/uploads")
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -41,6 +44,18 @@ def blogs():
 
     if request.method == 'GET':
         return open(BLOGS_FILE_PATH).read()
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        if 'file1' not in request.files:
+            return 'error'
+        file1 = request.files['file1']
+        path = os.path.join(UPLOAD_FOLDER_PATH, file1.filename)
+        file1.save(path)
+        return path
+    if request.method == 'GET':
+        return ""
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
---
+++ website/js/upload.js
@@ -0,0 +1,73 @@
+window.onload = function(){
+    $(".header").load("html/header.html");
+    $(".footer").load("html/footer.html");
+    if ($("html").height() > $(window).height()) {
+        $(".footer").removeClass("fixed-bottom")
+    } else {
+        $(".footer").addClass("fixed-bottom")
+    }
+
+    var fileURL = "https://placehold.co/600x400?text=Geen Foto Geupload"
+    var file;
+    const input = $(".preview")
+    const output = $(".preview-img")
+    input.on('change', function() {
+        file = input.prop('files')[0];
+        fileURL = URL.createObjectURL(file)
+        output.attr("src",fileURL);
+    });
+
+    $(".pbut").on("click",function(){
+        $(".pimg").attr("src",fileURL);
+        if ($(".ptitv").val()) {
+            $(".ptit").text($(".ptitv").val());
+        } else {
+            $(".ptit").text("Nog Geen Titel");
+        }
+        if ($(".pconv").val()) {
+            $(".pcont").text($(".pconv").val());
+        } else {
+            $(".pcont").text("Nog Geen Inhoud");
+        }
+        $(".pnam").text($(".pnamv").val());
+        $(".pdat").text(new Date().toISOString().slice(0, 10));
+        $("#modalpreview").modal()
+    })
+
+    function uploadIMG(img) {
+        var fd = new FormData();
+        fd.append('file1', img /*, optional filename */)
+
+        var req = jQuery.ajax({
+            url: 'http://flyyrin.pythonanywhere.com/upload', 
+            method: 'POST',
+            data: fd, // sends fields with filename mimetype etc
+            // data: aFiles[0], // optional just sends the binary
+            processData: false, // don't let jquery process the data
+            contentType: false // let xhr set the content type
+        });
+
+        // jQuery is promise A++ compatible and is the todays norms of doing things 
+        req.then(function(response) {
+           return response
+        }, function(xhr) {
+            return xhr
+        })
+        return req
+    }
+
+    async function uploadBlog() {
+        var data = {
+            "title":  $(".ptitv").val(),
+            "name":  $(".pnamv").val(),
+            "imgurl": await uploadIMG(file),
+            "date":  new Date().toISOString().slice(0, 10),
+            "content":  $(".pconv").val()
+        }
+        console.log(data)
+    }
+    $(".plaatsbut").on("click",function(){
+        uploadBlog()
+    })
+    
+}
