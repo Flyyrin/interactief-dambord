5e27b9d7ee2b579d5486ed790ce52f52333c3f5a
Vami220805 <90028381+Vami220805@users.noreply.github.com>
2023-01-12 12:15:23 +0100

    update

--- local/checkers.py
+++ local/checkers.py
@@ -1,10 +1,5 @@
 # sudo python3 local/checkers.py om te runnen
 # sudo pip3 install rpi_ws281x adafruit-circuitpython-neopixel voor neopixel install
-# pip3 install adafruit-circuitpython-neopixel
-# sudo pip3 install --upgrade adafruit-python-shell
-# wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
-# sudo python3 raspi-blinka.py
-# sudo pip3 install rpi_ws281x adafruit-circuitpython-neopixel
 # sudo python3 -m pip install --force-reinstall adafruit-blinka
 
 import sys
