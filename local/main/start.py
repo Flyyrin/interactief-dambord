264f8523d0185bc0aba54dfd977728c8dc26eba7
Rafael Lemmen <BouncePlayR@gmail.com>
2023-01-23 15:10:46 +0100

    -

--- local/main/start.py
+++ local/main/start.py
@@ -1,4 +1,8 @@
 import subprocess
 
-subprocess = subprocess.Popen("sudo python3 local/main/checkers.py", shell=True, stdout=subprocess.PIPE)
-subprocess_return = subprocess.stdout.read()
+if input("t") == "t":
+    subprocess = subprocess.Popen("sudo python3 local/test/test.py", shell=True, stdout=subprocess.PIPE)
+    subprocess_return = subprocess.stdout.read()
+else:
+    subprocess = subprocess.Popen("sudo python3 local/main/checkers.py", shell=True, stdout=subprocess.PIPE)
+    subprocess_return = subprocess.stdout.read()
