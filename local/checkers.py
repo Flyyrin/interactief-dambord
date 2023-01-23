de6753ae27340e388aa0fdc9ea5c58bb17428dda
Vami220805 <90028381+Vami220805@users.noreply.github.com>
2023-01-23 14:20:39 +0100

    update

--- local/checkers.py
+++ local/checkers.py
@@ -11,18 +11,31 @@ from LEDboardController import LED_setTile, LED_clear
 from gpiozero import Button
 from time import sleep
 
-joystick_onder = Button(5, pull_up = 0) #rood
-joystick_boven = Button(6, pull_up = 0) #zwart
-joystick_links = Button(13, pull_up = 0) #blauw
-joystick_rechts = Button(23, pull_up = 0) #groen
-joystick_button = Button(24, pull_up = 0) #wit
+joystick_onder1 = Button(5, pull_up = 0) #rood
+joystick_boven1 = Button(6, pull_up = 0) #zwart
+joystick_links1 = Button(13, pull_up = 0) #blauw
+joystick_rechts1 = Button(23, pull_up = 0) #groen
+joystick_button1 = Button(24, pull_up = 0) #wit
 #oranje en grijs op 5v!
 
-joystick_boven_pressed = False
-joystick_onder_pressed = False
-joystick_links_pressed = False
-joystick_rechts_pressed = False
-joystick_button_pressed = False
+joystick_boven1_pressed = False
+joystick_onder1_pressed = False
+joystick_links1_pressed = False
+joystick_rechts1_pressed = False
+joystick_button1_pressed = False
+
+joystick_onder2= Button(5, pull_up = 0) #rood
+joystick_boven2= Button(6, pull_up = 0) #zwart
+joystick_links2= Button(13, pull_up = 0) #blauw
+joystick_rechts2= Button(23, pull_up = 0) #groen
+joystick_button2= Button(24, pull_up = 0) #wit
+#oranje en grijs op 5v!
+
+joystick_boven2_pressed = False
+joystick_onder2_pressed = False
+joystick_links2_pressed = False
+joystick_rechts2_pressed = False
+joystick_button2_pressed = False
 
 # Game Functions
 def consolePrint(text):
@@ -459,52 +472,52 @@ def end(winner):
     start()
 
 def readController():
-    global joystick_boven_pressed
-    global joystick_onder_pressed
-    global joystick_links_pressed
-    global joystick_rechts_pressed
-    global joystick_button_pressed
+    global joystick_boven1_pressed
+    global joystick_onder1_pressed
+    global joystick_links1_pressed
+    global joystick_rechts1_pressed
+    global joystick_button1_pressed
     any_pressed = False
     while not any_pressed:
-        if joystick_boven.is_pressed:
-            if not joystick_boven_pressed:
-                joystick_boven_pressed = True
+        if joystick_boven1.is_pressed:
+            if not joystick_boven1_pressed:
+                joystick_boven1_pressed = True
                 any_pressed = True
                 return "up"
         else:
-            joystick_boven_pressed = False
+            joystick_boven1_pressed = False
 
-        if joystick_onder.is_pressed:
-            if not joystick_onder_pressed:
-                joystick_onder_pressed = True
+        if joystick_onder1.is_pressed:
+            if not joystick_onder1_pressed:
+                joystick_onder1_pressed = True
                 any_pressed = True
                 return "down"
         else:
-            joystick_onder_pressed = False
+            joystick_onder1_pressed = False
 
-        if joystick_links.is_pressed:
-            if not joystick_links_pressed:
-                joystick_links_pressed = True
+        if joystick_links1.is_pressed:
+            if not joystick_links1_pressed:
+                joystick_links1_pressed = True
                 any_pressed = True
                 return "left"
         else:
-            joystick_links_pressed = False
+            joystick_links1_pressed = False
             
-        if joystick_rechts.is_pressed:
-            if not joystick_rechts_pressed:
-                joystick_rechts_pressed = True
+        if joystick_rechts1.is_pressed:
+            if not joystick_rechts1_pressed:
+                joystick_rechts1_pressed = True
                 any_pressed = True
                 return "right"
         else:
-            joystick_rechts_pressed = False
+            joystick_rechts1_pressed = False
         
-        if joystick_button.is_pressed:
-            if not joystick_button_pressed:
-                joystick_button_pressed = True
+        if joystick_button1.is_pressed:
+            if not joystick_button1_pressed:
+                joystick_button1_pressed = True
                 any_pressed = True
                 return "press"
         else:
-            joystick_button_pressed = False
+            joystick_button1_pressed = False
 
 if __name__ == '__main__':
     start()
