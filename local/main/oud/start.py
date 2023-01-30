e47e32bcc9f8daacad00c05e48d1f7832e0814d2
vami220805 <vami220805@leerling.mosa-rt.be>
2023-01-30 15:04:20 +0100

    --

--- local/main/oud/start.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-if input("t") == "":
-    subprocess = subprocess.Popen("sudo python3 local/main/test.py", shell=True, stdout=subprocess.PIPE)
-    subprocess_return = subprocess.stdout.read()
-else:
-    subprocess = subprocess.Popen("sudo python3 local/main/checkers.py", shell=True, stdout=subprocess.PIPE)
-    subprocess_return = subprocess.stdout.read()
