5e2ff3437d4542ddf72ad19d054c143e32d8a51c
Rafael Lemmen <BouncePlayR@gmail.com>
2023-02-02 11:51:27 +0100

    -

--- local/main/brainstorm/gui/web.py
+++ local/main/brainstorm/gui/web.py
@@ -5,10 +5,20 @@ class Api:
     def __init__(self):
         pass
 
-    def start(self, name1, name2, color1, color2):
-        print(name1, name2, color1, color2)
+    # def destroy(self):
+    #     print("close")
+
+    def __init__(self):
+        self._window = None
+
+    def set_window(self, window):
+        self._window = window
+
+    def destroy(self):
+        self._window.destroy()
 
 if __name__ == '__main__':
     api = Api()
     window = webview.create_window('Start', os.path.join(os.getcwd(), "local/main/brainstorm/gui/start.html"), js_api=api)
+    api.set_window(window)
     webview.start()
