3594f44c31a77bca2f3e7026a5b6620732668e56
vami220805 <vami220805@leerling.mosa-rt.be>
2023-02-02 12:31:00 +0100

    update

--- local/main/brainstorm/gui/web.py
+++ local/main/brainstorm/gui/web.py
@@ -1,5 +1,6 @@
 import webview
 import os
+import subprocess
 
 # sudo apt install python3-gi python3-gi-cairo gir1.2-gtk-3.0 gir1.2-webkit2-4.0 pywebview
 
@@ -8,20 +9,16 @@ class Api:
     def __init__(self):
         pass
 
-    # def destroy(self):
-    #     print("close")
 
     def __init__(self):
-        self._window = None
+        pass
 
-    def set_window(self, window):
-        self._window = window
-
-    def destroy(self):
-        self._window.destroy()
+    def exit(self,nodig):
+        subprocess.Popen("killall python", shell=True, stdout=subprocess.PIPE)
+    
+    def start(self,nodig):
+        print("test")
 
 if __name__ == '__main__':
     api = Api()
     window = webview.create_window('Start', os.path.join(os.getcwd(), "local/main/brainstorm/gui/start.html"), js_api=api)
-    api.set_window(window)
-    webview.start()
