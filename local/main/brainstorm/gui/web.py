b92475f5178d40c30362ec8a4454980116a02a79
vami220805 <vami220805@leerling.mosa-rt.be>
2023-02-03 09:51:17 +0100

    -

--- local/main/brainstorm/gui/web.py
+++ local/main/brainstorm/gui/web.py
@@ -1,5 +1,5 @@
 # sudo apt install python3-gi python3-gi-cairo gir1.2-gtk-3.0 gir1.2-webkit2-4.0 pywebview
-
+# sudo python3 local/main/brainstorm/gui/web.py
 import webview
 import os
 import subprocess
@@ -14,6 +14,7 @@ class Api:
     def exit(self,nodig):
         print("Board uit")
         subprocess.Popen("killall python", shell=True, stdout=subprocess.PIPE)
+        quit()
     
     def start(self,cp):
         np1,np2,cp1,cp2 = cp.split("&")
