fba8e2ba4ef28cfefc86477d58389b256a3e1e3a
Rafael Lemmen <BouncePlayR@gmail.com>
2023-03-12 19:13:13 +0100

    removed css, js

---
+++ website2/code/start.py
@@ -0,0 +1,4 @@
+import subprocess
+
+subprocess = subprocess.Popen("sudo python3 /home/rpi/Documents/GIP-2022-2023/main/checker.py", shell=True, stdout=subprocess.PIPE)
+subprocess_return = subprocess.stdout.read()
--- website2/scripts/code.js
+++ website2/scripts/code.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
     function highlight() {
         hljs.highlightAll()
     }
-    $(".code-block").load("../code/checker.py");
+    $(".code-block-1").load("../code/checker.py");
+    $(".code-block-2").load("../code/webInterface.py");
+    $(".code-block-3").load("../code/controller.py");
+    $(".code-block-4").load("../code/start.py");
     setTimeout(highlight, 1000)
 });
