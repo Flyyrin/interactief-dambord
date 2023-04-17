63b3a38e6f57bbb0866e22af2ce00def17a61898
Rafael Lemmen <BouncePlayR@gmail.com>
2023-04-17 14:58:40 +0200

    -

---
+++ website/generate.py
@@ -0,0 +1,5 @@
+num = 0
+for x in range(8):
+    for y in range(8):
+        num += 1
+        print(f'<rect id="{num}" width="250" height="250" rx="65" ry="65" transform="matrix(0.75 0 0 0.75  {y*250 +35} {x*250 +35})"  fill="#00000000" filter="url(#glow)"/>')
