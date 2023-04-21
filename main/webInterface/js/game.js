f39afe014cb03192366bd1fda42204962ed8347f
Rafael Lemmen <BouncePlayR@gmail.com>
2023-04-21 11:50:11 +0200

    bot fixed

--- main/webInterface/js/game.js
+++ main/webInterface/js/game.js
@@ -5,7 +5,7 @@ window.onload = function() {;
     var cp2 = getParam('cp2');
 
     $(".player1").html(np1)
-    $(".player2").html(np2)
+    $(".player2").html(np2.replace("🤖", "<span>🤖</span>"))
     $(".player1").addClass(cp1+"-text")
     $(".player2").addClass(cp2+"-text")
 
