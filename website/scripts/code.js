cb1190d4485f49e3baf03a928f9a88bacc80a167
Rafael Lemmen <BouncePlayR@gmail.com>
2023-04-24 14:02:36 +0200

    -

--- website/scripts/code.js
+++ website/scripts/code.js
@@ -2,7 +2,12 @@ baseURL = "https://raw.githubusercontent.com/Flyyrin/GIP-2022-2023/main"
 
 var code = {
     "code-block-1": "main/json/config.json",
-    "code-block-2": "main/json/layout.json"
+    "code-block-2": "main/json/layout.json",
+    "code-block-3": "main/algorithm.py",
+    "code-block-4": "main/checker.py",
+    "code-block-5": "main/controller.py",
+    "code-block-6": "main/start.py",
+    "code-block-7": "main/webInterface.py"
 }
 
 $(document).ready(function() {
