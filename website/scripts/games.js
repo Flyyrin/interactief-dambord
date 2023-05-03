86a420650144108ac885d587c576d2e4bca95d28
Rafael Lemmen <BouncePlayR@gmail.com>
2023-05-03 17:11:03 +0200

    Update games.js

--- website/scripts/games.js
+++ website/scripts/games.js
@@ -54,7 +54,7 @@ async function loadData(data) {
             template = template.replace("tm", data[i]["time"])
             template = template.replace("pt", `${timeSince(data[i]["date"])} geleden`)
             // window.open('/history?game=gg', '_blank');
-            template = template.replace("oc", `window.open('/history?game=${data.length-data.indexOf(data[i])-1}', '_blank', 'top=150,left=${(window.innerWidth/2)-200},width=400,height=400');`)
+            template = template.replace("oc", `window.open('/history?game=${data.length-data.indexOf(data[i])-1}', '_blank', 'top=150,left=${(window.innerWidth/2)-150},width=300,height=500');`)
             $(".games").append(template);
         })
     }
