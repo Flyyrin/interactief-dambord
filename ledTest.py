65c320c76496a68150a7b1a600724dc84aa50e41
Rafael Lemmen <BouncePlayR@gmail.com>
2023-05-12 08:36:28 +0200

    Update ledTest.py

--- ledTest.py
+++ ledTest.py
@@ -6,4 +6,4 @@ pixels = neopixel.NeoPixel(board.D18, 128)
 
 while True:
     pixels[random.randint(0,127)] = (0,255,0)
-    pixels[random.randint(0,127)] = (0,255,0)
+    pixels[random.randint(0,127)] = (0,0,255)
