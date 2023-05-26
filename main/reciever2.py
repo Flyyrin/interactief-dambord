f920aa569496ac669ef8e2eda3e3df872e48ce10
Rafael Lemmen <BouncePlayR@gmail.com>
2023-05-26 11:27:57 +0200

    Update reciever2.py

--- main/reciever2.py
+++ main/reciever2.py
@@ -1,39 +1,34 @@
 import time
 import RPi.GPIO as GPIO
-import subprocess
+from rpi_rf import RFDevice
 
-PIN = 27  # GPIO pin connected to the data pin of RF receiver
+rf_pin = 17 # GPIO pin connected to the RF receiver data pin
+rf_device = RFDevice(rf_pin)
+rf_device.enable_rx()
+
+
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(PIN, GPIO.IN)
+GPIO.setup(18, GPIO.OUT) # GPIO pin connected to an LED
 
-def receive_code():
-    code = 0
-    
-    while GPIO.input(PIN) == 1:
-        pass
-    
-    # Wait for the start bit
-    while GPIO.input(PIN) == 0:
-        pass
-    
-    # Read the received bits
-    for i in range(0, 16):
-        while GPIO.input(PIN) == 1:
-            pass
-        code <<= 1
-        if GPIO.input(PIN) == 0:
-            code |= 1
-        while GPIO.input(PIN) == 0:
-            pass
-    
-    return code
+
 
 try:
     while True:
-        received_code = receive_code()
-        print("Received code:", received_code)
-        # Do something with the received code
-        
+        if rf_device.rx_code_timestamp != None:
+        # Get the received data
+        data = rf_device.rx_code
+        # Interpret the data as true/false
+        is_high = (chr(data) == 'H')
+        # Perform actions based on the signal
+        if is_high:
+        print("Received HIGH signal")
+        GPIO.output(18, GPIO.HIGH) # Turn on the LED
+        else:
+        print("Received LOW signal")
+        GPIO.output(18, GPIO.LOW) # Turn off the LED
+        rf_device.reset()
+        time.sleep(0.1)
 except KeyboardInterrupt:
-    GPIO.cleanup()
+ GPIO.cleanup()
+ rf_device.cleanup()
