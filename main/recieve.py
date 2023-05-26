3bf04c0ec7673dd1798a3eca55ef672bbff9c0f3
Rafael Lemmen <BouncePlayR@gmail.com>
2023-05-26 10:01:55 +0200

    Update recieve.py

--- main/recieve.py
+++ main/recieve.py
@@ -1,7 +1,29 @@
-git clone git://git.drogon.net/wiringPi
-cd wiringPi
-./build
+import RPi.GPIO as GPIO
+import time
+from rpi_rf import RFDevice
 
-git clone –recursive git://github.com/ninjablocks/433Utils.git
-cd 433Utils/RPi_utils
-make
+GPIO.setmode(GPIO.BCM)
+
+rf_receiver = RFDevice(17)
+rf_receiver.enable_rx()
+
+timestamp = None
+
+while True:
+    if rf_receiver.rx_code_timestamp != timestamp:
+        timestamp = rf_receiver.rx_code_timestamp
+        received_data = rf_receiver.rx_code
+
+        if received_data == "KEY1":
+            # Handle KEY1 action here
+            print("Received KEY1")
+
+        elif received_data == "KEY2":
+            # Handle KEY2 action here
+            print("Received KEY2")
+
+        # Add more key conditions as needed
+
+    time.sleep(0.01)
+
+rf_receiver.cleanup()
