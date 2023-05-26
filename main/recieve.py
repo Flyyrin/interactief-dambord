6b4b6858dd7ccb7c8b9fbaefa01cbb40aefbcb87
Rafael Lemmen <BouncePlayR@gmail.com>
2023-05-26 09:51:49 +0200

    Update recieve.py

--- main/recieve.py
+++ main/recieve.py
@@ -1,1 +1,7 @@
-# https://securipi.co.uk/remote-433-receivers.pdf
+git clone git://git.drogon.net/wiringPi
+cd wiringPi
+./build
+
+git clone –recursive git://github.com/ninjablocks/433Utils.git
+cd 433Utils/RPi_utils
+make
