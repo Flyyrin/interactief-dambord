a0d705cf33fc1a23b3389d44dc3736dba1aeeccc
Rafael Lemmen <BouncePlayR@gmail.com>
2023-06-01 11:55:43 +0200

    Update recieve.cs

--- main/remote/recieve.cs
+++ main/remote/recieve.cs
@@ -4,44 +4,20 @@
 RH_ASK driver;
 
 void setup() {
-  Serial.begin(9600);     // Initialize serial communication for debugging
+  Serial.begin(9600);
   if (!driver.init()) {
-    Serial.println("RadioHead initialization failed");
+    Serial.println("initialization failed");
   }
 }
 
 void loop() {
-  uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];  // Buffer to hold received data
-  uint8_t buflen = sizeof(buf);  // Length of received data buffer
+  uint8_t buf = "";
+  uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
+  uint8_t buflen = sizeof(buf);
 
-  if (driver.recv(buf, &buflen)) {  // Check if data is received
+  if (driver.recv(buf, &buflen)) {  
     Serial.print("Received: ");
-    Serial.println((char *)buf);  // Print the received data
+    Serial.println((char *)buf);
     driver.resetAvailable();
-  }
-}
-
-
-#include <RH_ASK.h>
-#include <SPI.h>
-
-RH_ASK driver;
-
-void setup() {
-  Serial.begin(9600);     // Initialize serial communication for debugging
-  if (!driver.init()) {
-    Serial.println("RadioHead initialization failed");
-  }
-}
-
-void loop() {
-  uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];  // Buffer to hold received data
-  uint8_t buflen = sizeof(buf);  // Length of received data buffer
-
-  if (driver.recv(buf, &buflen)) {  // Check if data is received
-    Serial.print("Received: ");
-    Serial.println((char *)buf);  // Print the received data
-    memset(buf, 0, buflen);  // Clear the buffer
-    buflen = sizeof(buf);  // Reset the buffer length
   }
 }
