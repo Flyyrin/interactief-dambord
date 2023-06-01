8ec45520ce5a6abde632cf8e3d363a73953698b6
Rafael Lemmen <BouncePlayR@gmail.com>
2023-06-01 12:00:26 +0200

    Update recieve.cs

--- main/remote/recieve.cs
+++ main/remote/recieve.cs
@@ -11,8 +11,7 @@ void setup() {
 }
 
 void loop() {
-  uint8_t buf = "";
-  uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
+  uint8_t buf[RH_ASK_MAX_MESSAGE_LEN] = {0};
   uint8_t buflen = sizeof(buf);
 
   if (driver.recv(buf, &buflen)) {  
