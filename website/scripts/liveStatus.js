b2e42c73ef0e8ddb33700718b614e801c4d9cd67
Rafael Lemmen <BouncePlayR@gmail.com>
2025-02-25 11:49:35 +0100

    Update liveStatus.js

--- website/scripts/liveStatus.js
+++ website/scripts/liveStatus.js
@@ -2,9 +2,14 @@ $(document).ready(function () {
   const server = "https://flyyrin.pythonanywhere.com/game";
 
   function update() {
-    $(".liveStatus").text("Live Game (not active)");
-    $(".liveStatusData").hide();
-    $(".liveStatusDivider").hide();
+    $(".liveStatus").text("Live Game (active)");
+    $(".liveStatusData").show();
+    $(".liveStatusDivider").show();
+    $(".liveStatusData1").text("Rafael");
+    $(".liveStatusData2").text("Michael");
+    $(".liveStatusData1").addClass("red-text");
+    $(".liveStatusData2").addClass("purple-text");
+    $(".liveStatusDatat").text(mmss);
   }
 
   update();
